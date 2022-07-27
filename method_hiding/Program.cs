eab2a67e97b39c759f96f901c66dd0aeef972ef4
admin <admin@CHETUIWK1860>
2022-07-27 14:09:58 +0530

    Add project files.

---
+++ method_hiding/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace method_hiding
+{
+    class Dummy
+    {
+        public void Show()
+        {
+            Console.WriteLine("this is parent class method");
+        }
+    }
+
+    class demo : Dummy
+    {
+        public   new void Show()
+        {
+            Console.WriteLine("this is child class method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            demo D = new demo();
+            D.Show();
+            Console.ReadLine();
+        }
+    }
+}
